802ab793f0eed342282cba9f60c03f58952675a4
Shreya <91543981+shreya71@users.noreply.github.com>
2022-05-12 07:53:02 -0700

    Add files via upload

---
+++ Algoritms/Sieve of Eratosthenes/Prime factorization of a number n.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+void prime_factor(int n)
+{
+    int spf[100] = {0};
+    for(int i = 2; i <= n; i++)
+    {
+        spf[i] = i ;
+    }
+    for(int i = 2; i<= n; i++)
+    {
+        if(spf[i]==i)
+        {
+            for(int j = i*i; j <= n; j += i)
+            {
+                if(spf[j] == j)
+                spf[j] = i;
+            }
+        }
+    }
+    while(n!=1)
+    {
+        cout << spf[n] << " " ;
+        n = n/spf[n] ;
+    }
+    cout << endl;
+}
+int main()
+{
+   int n;
+   cin >> n;
+   prime_factor(n);
+}
