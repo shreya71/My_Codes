768d507eee2aae95545b42cece0c35603cd7f24c
Shreya <91543981+shreya71@users.noreply.github.com>
2022-06-14 01:05:21 +0530

    Create LL. FLoyd's Cycle finding algo.cpp

---
+++ Algoritms/LL. FLoyd's Cycle finding algo.cpp
@@ -0,0 +1,9 @@
+/*******************************************************************************************
+Basically we have two pointers in this algo, one fast and other slow.
+The time when faster moves by 2 nodes slower moves 1 node. So till the 2nd pointer reaches NULL or the last node of the LL
+(depending on even and odd nodes present respectively) the first pointer reaches the midlle elements as per the conditions. 
+
+1. Used to find middle elment
+2. or used in cyclic LL
+3. etc etc
+******************************************************************************************/
